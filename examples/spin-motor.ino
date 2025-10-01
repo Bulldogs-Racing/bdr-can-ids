813bfd49500ad8c1c5e8d1ce2f9859c6a5a326ce
CrankyTitanO7 <nedajtheawesome@gmail.com>
2025-10-01 02:11:28 -0400

    update examples from bdr original files

--- examples/spin-motor.ino
+++ examples/spin-motor.ino
@@ -1,5 +1,7 @@
 #include <FlexCAN_T4.h>
+
 #include <bdrcanlib.h>
+
 #include <Arduino.h>
 
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0;
@@ -14,8 +16,8 @@ FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0;
 #define AVG(a, b) ((a + b) / 2)
 
 
-#define DRIVE_ENABLE_ON 0x01                // payload to enable drive
-#define DRIVE_ENABLE_LEN 1                  // length of drive enable msg
+#define enable_drive_ON 0x01                // payload to enable drive
+#define enable_drive_LEN 1                  // length of drive enable msg
 
 #define SET_CURRENT_LEN 2                   // length of set current msg
 
@@ -29,7 +31,10 @@ FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> Can0;
 #define POT_PIN_TWO 233                     //redifine pins
 #define READY_MSG 0x07FE                    // id for ready to drive msg (NOT INCLUDED IN PROTOCOL DOC)
 
-void drive_enable();
+#define SET_CURRENT_ID 0x011F
+#define SET_CURRENT_LEN 2
+
+void enable_drive();
 bool ready = false;
 
 void setup(void) {
@@ -45,7 +50,7 @@ void setup(void) {
     while (!ready) Can0.events();
     Can0.disableFIFOInterrupt();
     Can0.onReceive(NULL);
-    drive_enable();
+    enable_drive();
 }
 
 void ready_to_drive(const CAN_message_t &msg) {
@@ -54,13 +59,28 @@ void ready_to_drive(const CAN_message_t &msg) {
 }
 
 
-void drive_enable(){
+void enable_drive(){
     CAN_message_t msg;
-    msg.id = DRIVE_ENABLE_ID;
-    msg.len = DRIVE_ENABLE_LEN; 
-    for (uint8_t j = 0; j < msg.len; j++ ) msg.buf[j] = DRIVE_ENABLE_ON; // set the payload to the length
+    msg.id = Drive_Enable.id;
+    msg.len = Drive_Enable.length; 
+    for (uint8_t j = 0; j < msg.len; j++ ) msg.buf[j] = enable_drive_ON; // set the payload to the length
     Can0.write(msg);
 }
+
+void set_current(int16_t amps) {
+    // amps: signed value in A_pk
+    int16_t scaled = amps * 10;   // scale according to DBC
+
+    CAN_message_t msg;
+    msg.id = Set_AC_Current.id;   // from your bdrcanlib
+    msg.len = Set_AC_Current.length;
+
+    msg.buf[0] = scaled & 0xFF;         // LSB
+    msg.buf[1] = (scaled >> 8) & 0xFF;  // MSB
+
+    Can0.write(msg);
+}
+
 
 uint8_t curr_val[SET_CURRENT_LEN];
 
@@ -68,17 +88,10 @@ uint8_t curr_val[SET_CURRENT_LEN];
 void loop() {
     long long start_time = millis();
 
-    CAN_message_t msg;
-    msg.id = SET_CURRENT_ID;
-    msg.len = SET_CURRENT_LEN;
-
-    for (uint8_t i = 0; i < msg.len; i++ ) {
-      Serial.println(curr_val[i]);
-      msg.buf[i] = curr_val[i];
-    }
-    Can0.write(msg);
+    set_current(150);  // request 150 A
 
     long long end_time = millis();
     if ((end_time - start_time) < ITERATION_TIME) 
-      delay(ITERATION_TIME - (end_time - start_time));  // stall until iteration time is hit
+        delay(ITERATION_TIME - (end_time - start_time));
 }
+
